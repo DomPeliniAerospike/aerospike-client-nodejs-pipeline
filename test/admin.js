74ba1dde11c5cd6a024bff381a966672ab48dd0d
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-02-08 01:14:19 -0700

    pipe

--- test/admin.js
+++ test/admin.js
@@ -34,7 +34,7 @@ function wait (ms) {
 }
 
 context('admin commands', async function () {
-  if (helper.config.user !== 'admin') {
+  if (helper.config.user !== 'superuser') {
     return
   }
   const client = helper.client
