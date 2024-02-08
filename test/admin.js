ce89b9ada3b303834123e337c8688e05e0fb37bb
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-02-08 07:34:30 -0700

    de

--- test/admin.js
+++ test/admin.js
@@ -460,7 +460,7 @@ context('admin commands', async function () {
   describe('Client#changePassword()', function () {
     it('Changes password for user', async function () {
       client.changePassword(username1, 'password350', null)
-      await wait(waitMs + 1200)
+      await wait(waitMs + 30000)
       const config = {
         hosts: options.host + ':' + options.port,
         user: username1,
@@ -472,7 +472,7 @@ context('admin commands', async function () {
 
     it('With policy', async function () {
       client.changePassword(username2, 'password250', policy)
-      await wait(waitMs + 1200)
+      await wait(waitMs + 3000)
       const config = {
         hosts: options.host + ':' + options.port,
         user: username2,
